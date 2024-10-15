272ed7d159196c25099b600e7bcd6ba8100e3c76
JosePonferrada <joseponfe10@gmail.com>
2024-10-15 13:00:49 +0200

    feat(ES6): Apuntes added

---
+++ Apuntes/JS6/Módulos/main.js
@@ -0,0 +1,5 @@
+import sumar from "./modulo.js"; // Puedes llamar a la función por el nombre que quieras porque no tiene nombre
+
+// Cuando yo quiero importar una función que tiene un nombre, deberemos llamarla de esta manera: import {sumar}
+
+sumar (5, 7);
