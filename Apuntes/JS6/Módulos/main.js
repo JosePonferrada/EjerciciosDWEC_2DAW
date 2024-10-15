fa7d137fbb1b8b23c62bd1e0f3fecf410e38b0c0
JosePonferrada <joseponfe10@gmail.com>
2024-10-15 14:02:00 +0200

    feat(ES6): examples added

--- Apuntes/JS6/Módulos/main.js
+++ Apuntes/JS6/Módulos/main.js
@@ -1,5 +1,8 @@
 import sumar from "./modulo.js"; // Puedes llamar a la función por el nombre que quieras porque no tiene nombre
 
 // Cuando yo quiero importar una función que tiene un nombre, deberemos llamarla de esta manera: import {sumar}
+// Cuando tengo varios exports y un export default, la default se importará fuera de las llaves
+
+// También se puede exportar un objeto con todas las funciones que quiera y al ser un export default lo llamamos como queramos
 
 sumar (5, 7);
